4835157cbad193619fc02e0b84be265fdf2b5db2
Adam Kakes <adamkakes@gmail.com>
2021-10-31 18:16:30 +0100

    Přidejte soubory projektu.

---
+++ nejvetsiCislo/nejvetsiCislo.cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+
+int main(void) 
+{
+	int a = 1, b = 3, c = 5;
+	int max;
+	if (a == b){
+		if (a == c)
+		{
+			max = c;
+		}
+		else
+		{
+			if (c > a)
+			{
+				max = c;
+			}
+			else
+			{
+				max = a;
+			}
+		}
+	}
+	else
+	{
+		if (a > b)
+		{
+			if (a == c)
+			{
+				max = c;
+			}
+			else
+			{
+				if (c > a)
+				{
+					max = c;
+				}
+				else
+				{
+					max = a;
+				}
+			}
+		}
+		else
+		{
+			if (b == c)
+			{
+				max = c;
+			}
+			else
+			{
+				if (c > b)
+				{
+					max = c;
+				}
+				else
+				{
+					max = b;
+				}
+			}
+		}
+	}
+	printf("nejvetsi cislo je %d\n", max);
+}
